44c6935df2b86ac8f12280db943337715ccf552b
Sai-Gitnoob <saitukrul@gmail.com>
2026-05-22 13:35:56 +0530

    Day 2

---
+++ 01.Basics/1.printing.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+
+  string x = "Hello world";
+  cout<<x ;
+  string name;
+  cin>>name; // Takes input 
+  cout<<"Hello"<<name; //Displays the output 
+
+  return 0;
+}
---
+++ 01.Basics/2.Constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+int main(){
+  const int a = 12 ;
+  int b = 15  ;
+  cout<<a << b ;
+  b = 10 ;
+  cout << a << b ;
+  return 0 ;
+}
