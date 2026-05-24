f40e8edd9fdefe2532474cf605da0e52f73694cd
Sai-Gitnoob <saitukrul@gmail.com>
2026-05-24 14:15:14 +0530

    24-May-2026

---
+++ 01.Basics/5.Constants.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+  const int gravity = 9.8;
+  const int pie = 3.1415;
+  cout<<"This is the value of the gravity on earth"<<" "<<gravity<<endl;
+  cout<<"This is value of pie"<<" "<<pie<<endl;
+  return 0 ;
+}
