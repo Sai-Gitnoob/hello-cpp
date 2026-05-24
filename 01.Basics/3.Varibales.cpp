2904fed83f8db5e7a67440cb6843d8acc36d0eae
Sai-Gitnoob <saitukrul@gmail.com>
2026-05-24 13:57:28 +0530

    24-may-2026

---
+++ 01.Basics/3.Varibales.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+  char nameI = 'x';
+  int age = 23;
+  string name ="Ninen";
+  bool isFresher = true ;
+  float salary = 1500.50 ;
+  double performance_score = 3.43449999999;
+
+  cout<<nameI<<age<<name<<isFresher<<salary<<performance_score<<endl;
+  return 0 ;
+
+}
